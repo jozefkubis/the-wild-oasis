86dd06a073ee13b178ea7751cafe03a1279e363d
jozef kubis <kubis.jozef@outlook.com>
2024-11-10 12:50:34 +0100

    API Side fltering and sorting

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,23 @@
 import { getToday } from "../utils/helpers"
 import supabase from "./supabase"
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
     )
+
+  // MARK: FILTERING
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value)
+
+  // MARK: SORTING
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    })
+
+  const { data, error } = await query
 
   if (error) {
     console.error(error)
