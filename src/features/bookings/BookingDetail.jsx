f95215481501650058ce47aad7c3bb1aebe27fd9
JozefK <164421631+jozefkubis@users.noreply.github.com>
2024-11-17 20:34:40 +0100

    Building the Single Booking Page

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -1,38 +1,43 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
-import BookingDataBox from "./BookingDataBox";
-import Row from "../../ui/Row";
-import Heading from "../../ui/Heading";
-import Tag from "../../ui/Tag";
-import ButtonGroup from "../../ui/ButtonGroup";
-import Button from "../../ui/Button";
-import ButtonText from "../../ui/ButtonText";
+import BookingDataBox from "./BookingDataBox"
+import Row from "../../ui/Row"
+import Heading from "../../ui/Heading"
+import Tag from "../../ui/Tag"
+import ButtonGroup from "../../ui/ButtonGroup"
+import Button from "../../ui/Button"
+import ButtonText from "../../ui/ButtonText"
 
-import { useMoveBack } from "../../hooks/useMoveBack";
+import { useMoveBack } from "../../hooks/useMoveBack"
+import { useBooking } from "./useBooking"
+import Spinner from "../../ui/Spinner"
 
 const HeadingGroup = styled.div`
   display: flex;
   gap: 2.4rem;
   align-items: center;
-`;
+`
 
 function BookingDetail() {
-  const booking = {};
-  const status = "checked-in";
+  const { booking, isLoading } = useBooking()
 
-  const moveBack = useMoveBack();
+  const moveBack = useMoveBack()
+
+  if (isLoading) return <Spinner />
+
+  const { status, id: bookingId } = booking
 
   const statusToTagName = {
     unconfirmed: "blue",
     "checked-in": "green",
     "checked-out": "silver",
-  };
+  }
 
   return (
     <>
       <Row type="horizontal">
         <HeadingGroup>
-          <Heading as="h1">Booking #X</Heading>
+          <Heading as="h1">Booking #{bookingId}</Heading>
           <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
         </HeadingGroup>
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
@@ -46,7 +51,7 @@ function BookingDetail() {
         </Button>
       </ButtonGroup>
     </>
-  );
+  )
 }
 
-export default BookingDetail;
+export default BookingDetail
--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -1,18 +1,21 @@
-import styled from "styled-components";
-import { format, isToday } from "date-fns";
+import styled from "styled-components"
+import { format, isToday } from "date-fns"
 
-import Tag from "../../ui/Tag";
-import Table from "../../ui/Table";
+import Tag from "../../ui/Tag"
+import Table from "../../ui/Table"
 
-import { formatCurrency } from "../../utils/helpers";
-import { formatDistanceFromNow } from "../../utils/helpers";
+import { formatCurrency } from "../../utils/helpers"
+import { formatDistanceFromNow } from "../../utils/helpers"
+import Menus from "../../ui/Menus"
+import { HiEye } from "react-icons/hi2"
+import { useNavigate } from "react-router-dom"
 
 const Cabin = styled.div`
   font-size: 1.6rem;
   font-weight: 600;
   color: var(--color-grey-600);
   font-family: "Sono";
-`;
+`
 
 const Stacked = styled.div`
   display: flex;
@@ -27,12 +30,12 @@ const Stacked = styled.div`
     color: var(--color-grey-500);
     font-size: 1.2rem;
   }
-`;
+`
 
 const Amount = styled.div`
   font-family: "Sono";
   font-weight: 500;
-`;
+`
 
 function BookingRow({
   booking: {
@@ -48,11 +51,13 @@ function BookingRow({
     cabins: { name: cabinName },
   },
 }) {
+  const navigate = useNavigate()
+
   const statusToTagName = {
     unconfirmed: "blue",
     "checked-in": "green",
     "checked-out": "silver",
-  };
+  }
 
   return (
     <Table.Row>
@@ -79,8 +84,20 @@ function BookingRow({
       <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
 
       <Amount>{formatCurrency(totalPrice)}</Amount>
+
+      <Menus.Menu>
+        <Menus.Toggle id={bookingId} />
+        <Menus.List id={bookingId}>
+          <Menus.Button
+            icon={<HiEye />}
+            onClick={() => navigate(`/bookings/${bookingId}`)}
+          >
+            See details
+          </Menus.Button>
+        </Menus.List>
+      </Menus.Menu>
     </Table.Row>
-  );
+  )
 }
 
-export default BookingRow;
+export default BookingRow
