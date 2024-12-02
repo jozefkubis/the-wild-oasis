79c2017f60c32b235ca6bbe128278d646136cbde
jozef kubis <kubis.jozef@outlook.com>
2024-12-02 20:38:45 +0100

    Error Boundaries

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import styled from "styled-components"
+import Heading from "./Heading"
+import GlobalStyles from "../styles/GlobalStyles"
+import Button from "./Button"
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -7,7 +10,7 @@ const StyledErrorFallback = styled.main`
   align-items: center;
   justify-content: center;
   padding: 4.8rem;
-`;
+`
 
 const Box = styled.div`
   /* Box */
@@ -28,4 +31,21 @@ const Box = styled.div`
     margin-bottom: 3.2rem;
     color: var(--color-grey-500);
   }
-`;
+`
+
+export default function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong 😒</Heading>
+          <p>{error.message}</p>
+          <Button size="large" onClick={resetErrorBoundary}>
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  )
+}
